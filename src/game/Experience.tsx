121ef40de85c312d7704c648824a819c74488d9d
Joel Ymele <joel.ymele@gmail.com>
2024-06-09 16:07:38 -0400

    Added blender models

--- src/game/Experience.tsx
+++ src/game/Experience.tsx
@@ -1,10 +1,8 @@
 import { Canvas } from "@react-three/fiber";
-
 import Lighting from "./utils/Lighting";
 import Floor from "./components/Floor";
 import { Physics } from "@react-three/rapier";
 import Player from "./components/Player";
-import { OrbitControls } from "@react-three/drei";
 
 interface Props {}
 
@@ -20,9 +18,8 @@ function Experience(props: Props) {
         position: [-1, 12, 15],
       }}
     >
-      <OrbitControls />
       <Lighting />
-      <Physics debug gravity={[0, -1.2, 0]}>
+      <Physics gravity={[0, -1.2, 0]}>
         <Player />
         <Floor />
       </Physics>
--- src/game/components/Player.tsx
+++ src/game/components/Player.tsx
@@ -1,6 +1,6 @@
-import { RefObject, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import Ecctrl, { useGame } from "ecctrl";
+import Ecctrl, { EcctrlAnimation, useGame } from "ecctrl";
+import PlayerModel from "./models/PlayerModel";
 
 interface Props {}
 
@@ -9,11 +9,31 @@ function Player(props: Props) {
 
   useFrame(() => {});
 
+  const animationSet = {
+    idle: "Walk",
+    walk: "Walk",
+    run: "Walk",
+    jump: "Walk",
+    jumpIdle: "Walk",
+    jumpLand: "Walk",
+    fall: "Walk", // This is for falling from high sky
+    // Currently support four additional animations
+    // action1: "Wave",
+    // action2: "Dance",
+    // action3: "Cheer",
+    // action4: "Attack(1h)",
+  };
+
   return (
     <>
-      {/* <RigidBody type="kinematicVelocity" position-y={5.5}> */}
-      <Ecctrl>
-        <group scale={0.5}>
+      <Ecctrl animated>
+        <EcctrlAnimation
+          characterURL="/models/player.glb"
+          animationSet={animationSet}
+        >
+          <PlayerModel scale={0.2} />
+        </EcctrlAnimation>
+        {/* <group scale={0.5}>
           <mesh castShadow>
             <boxGeometry args={[0.5, 0.5, 1]} />
             <meshLambertMaterial color="hotpink" reflectivity={1} />
@@ -26,9 +46,8 @@ function Player(props: Props) {
             <capsuleGeometry args={[0.25, 0.25, 2, 16]} />
             <meshLambertMaterial color="hotpink" reflectivity={1} />
           </mesh>
-        </group>
+        </group> */}
       </Ecctrl>
-      {/* </RigidBody> */}
     </>
   );
 }
